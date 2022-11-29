bc42e44f0b9330559f1dbc18bb94386c84503a7e
Herman F. Autore <herman@ufl.edu>
2022-11-29 00:15:17 -0500

    Create makeDirTemplate.py

---
+++ makeDirTemplate.py
@@ -0,0 +1,26 @@
+"""
+Create a working directory from template. THe structure of the directory to be made is as follows:
+
+New Directory
+├── Code
+├── Data
+│   ├── Input
+│   └── Output
+├── Logs
+├── SQL
+└── .env
+
+where "New Directory" is the name of the directory to be created
+"""
+
+
+def main(dir_path):
+    """
+    """
+    # TODO create files and directories as outlined in the above docstring.
+
+
+if __name__ == "__main__":
+    # TODO Add argument parsing with "argparse"
+    # TODO Add logging with "logging" package
+    pass
