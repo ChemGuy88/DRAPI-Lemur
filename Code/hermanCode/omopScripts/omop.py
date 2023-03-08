881845f9df43a4e5aa4e493a1c1a9031d179e028
Herman F. Autore <herman@ufl.edu>
2023-03-08 16:17:29 -0500

    Add `interpretPath`

--- Code/hermanCode/omopScripts/omop.py
+++ Code/hermanCode/omopScripts/omop.py
@@ -2,6 +2,8 @@
 Stuff for OMOP
 """
 
+import os
+import sys
 import yaml
 from pathlib import Path
 
@@ -16,9 +18,25 @@ def editConfig(inputPath: Path, outputPath: Path, timestamp):
     deidentified_file_location = Path(configFile["data_output"]["deidentified_file_location"]).joinpath(timestamp)
     mapping_location = Path(configFile["data_output"]["mapping_location"]).joinpath(timestamp)
 
-    configFile["data_output"]["identified_file_location"] = identified_file_location.__str__() + "/"
-    configFile["data_output"]["deidentified_file_location"] = deidentified_file_location.__str__() + "/"
-    configFile["data_output"]["mapping_location"] = mapping_location.__str__() + "/"
+    sep = os.sep
+
+    configFile["data_output"]["identified_file_location"] = identified_file_location.__str__() + sep
+    configFile["data_output"]["deidentified_file_location"] = deidentified_file_location.__str__() + sep
+    configFile["data_output"]["mapping_location"] = mapping_location.__str__() + sep
 
     with open(outputPath, "w") as file:
         yaml.dump(configFile, file)
+
+
+def interpretPath(pathAsString: str) -> str:
+    """
+    Makes sure path separators are appropriate for the current operating system.
+    """
+    operatingSystem = sys.platform
+    if operatingSystem == "darwin":
+        newPathAsString = pathAsString.replace("\\", "/")
+    elif operatingSystem == "win32":
+        newPathAsString = pathAsString
+    else:
+        raise Exception("Unsupported operating system")
+    return newPathAsString
