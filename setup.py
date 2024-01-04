daf6753f15d04e0c1d54cc124d4aed903a52cd2a
Herman F. Autore <herman@ufl.edu>
2024-01-04 18:37:10 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(name='drapiPackage',
+      packages=['drapiPackage'],
+      version='v1.0.0',  # Ideally should be same as your GitHub release tag varsion
+      description='Data Request API for the Integrated Data Repository Research Services of University of Florida.',
+      author='Herman Autore',
+      author_email='hf.autore+drapi@gmail.com',
+      url='https://github.com/ChemGuy88/hermanCode/archive/refs/tags/v1.0.0.tar.gz',
+      download_url='download link you saved',
+      keywords=['CTSI',
+                'Clinical and Translational Science Institute',
+                  'IDR',
+                  'Integrated Data Repository',
+                  'Integrated Data Repository Research Services',
+                  'ODSRI',
+                  'Office of Data Science and Research Implementation',
+                  'Shands',
+                  'Sloth',
+                  'UF',
+                  'UF Health',
+                  'UFHealth',
+                  'University of Florida'],
+      classifiers=[]
+      )
