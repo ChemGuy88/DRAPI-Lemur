94ab90936d38d3bd48008efcc36cec0ede803433
Herman Autore <herman@ufl.edu>
2024-01-04 14:38:07 -0500

    Added usage instructions.

--- code/tree.py
+++ code/tree.py
@@ -1,6 +1,22 @@
 """
 Utility program to visualize the structure of a directory and its contents in the form of a tree. Compare this to the Linux built-in `tree`.
 """
+
+USAGE_INSTRUCTIONS = """Usage instructions
+
+To use this program, just enter in the command line:
+
+`python tree.py directory_path`
+
+where `directory_path` is the path of the directory you want the tree of. If you are in the directory you want the tree for, then you just do
+
+`python tree.py .`
+
+Note the above example assumes "tree.py" is in the same directory that you are operating on. If the program is in another directory, you must type its path as below
+
+`python path/to/tree.py directory_path`
+"""
+
 # NOTE The function `tree` is copied from, and is also available in the `drapi` module.
 
 import argparse
@@ -73,7 +89,12 @@ if __name__ == "__main__":
                         default=1000,
                         type=int)
 
-    args = parser.parse_args()
+    if not len(sys.argv) > 1:
+        parser.print_usage()
+        print(USAGE_INSTRUCTIONS)
+        sys.exit(0)
+    else:
+        args = parser.parse_args()
 
     dir_path = args.dir_path
     level = args.level
@@ -85,5 +106,3 @@ if __name__ == "__main__":
          limit_to_directories=limit_to_directories,
          length_limit=length_limit)
 
-    if not len(sys.argv) > 1:
-        parser.print_usage()
