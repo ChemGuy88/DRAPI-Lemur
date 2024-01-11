b54a6f3483d4b4940f838885b6b142ff0b4d6c49
Herman F. Autore <herman@ufl.edu>
2024-01-10 20:07:27 -0500

    Add package data, conform to standards

--- setup.py
+++ setup.py
@@ -1,25 +1,26 @@
 from distutils.core import setup
+from setuptools import find_namespace_packages
 
-setup(name='drapiPackage',
-      packages=['drapiPackage'],
-      version='v1.0.0',  # Ideally should be same as your GitHub release tag varsion
+setup(name='drapi-lemur',
+      package_data={"drapiPackage.sql": ["*"],
+                    "drapiPackage.templates": ["*"]},
+      package_dir={"": "drapiPackage"},
+      packages=find_namespace_packages(where="drapiPackage"),
+      version='1.0.0',
       description='Data Request API for the Integrated Data Repository Research Services of University of Florida.',
       author='Herman Autore',
       author_email='hf.autore+drapi@gmail.com',
       url='https://github.com/ChemGuy88/hermanCode/archive/refs/tags/v1.0.0.tar.gz',
-      download_url='download link you saved',
       keywords=['CTSI',
                 'Clinical and Translational Science Institute',
-                  'IDR',
-                  'Integrated Data Repository',
-                  'Integrated Data Repository Research Services',
-                  'ODSRI',
-                  'Office of Data Science and Research Implementation',
-                  'Shands',
-                  'Sloth',
-                  'UF',
-                  'UF Health',
-                  'UFHealth',
-                  'University of Florida'],
-      classifiers=[]
-      )
+                'IDR',
+                'Integrated Data Repository',
+                'Integrated Data Repository Research Services',
+                'ODSRI',
+                'Office of Data Science and Research Implementation',
+                'Shands',
+                'Sloth 🦥',
+                'UF',
+                'UF Health',
+                'UFHealth',
+                'University of Florida'])
