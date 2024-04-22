f38f7e6371b151e9da9bb2435dbbfc21910f7a73
Herman Autore <herman@ufl.edu>
2024-04-22 18:36:22 -0400

    v1.0.35, pass path as string, not PosixPath object

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from distutils.core import setup
 from pathlib import Path
 from setuptools import find_namespace_packages
 
-SCRIPTS = sorted([fpath for fpath in Path("./src/scripts").iterdir()])
+SCRIPTS = sorted([str(fpath) for fpath in Path("./src/scripts").iterdir()])
 
 setup(name='drapi-lemur',
       package_dir={"": "src"},
       include_package_data=True,
       packages=find_namespace_packages(where="src"),
       scripts=SCRIPTS,
-      version='1.0.34',
+      version='1.0.35',
       description='Data Request API for the Integrated Data Repository Research Services of University of Florida.',
       long_description="Data Request API for the Integrated Data Repository Research Services of University of Florida.",
       author='Herman Autore',
