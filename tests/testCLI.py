2cb456ac01d0b5e32814edb93ffc8f11955406e1
Herman Autore <herman@ufl.edu>
2024-06-20 10:02:50 -0400

    Update test

--- tests/testCLI.py
+++ tests/testCLI.py
@@ -6,21 +6,18 @@ import argparse
 # Third-party packages
 pass
 # First-party packages
-pass
+from drapi.code.drapi.cli_parsers import parse_string_to_boolean
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--DOWNLOAD_DATA",
-                        required=True,
-                        type=lambda value: True if value.lower() == "true" else False if value.lower() == "false" else None)
+    parser.add_argument("--boolean",
+                        default=False,
+                        type=parse_string_to_boolean)
 
     argNamespace = parser.parse_args()
 
     # Parsed arguments: Main: Multiple query option
-    DOWNLOAD_DATA = argNamespace.DOWNLOAD_DATA
+    boolean = argNamespace.boolean
 
-    if isinstance(DOWNLOAD_DATA, type(None)):
-        parser.error("--DOWNLOAD_DATA must be one of {{True, False}}.")
-
-    print(DOWNLOAD_DATA)
+    print(boolean)
