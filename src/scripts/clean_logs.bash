be7638344921b40e3158680693884b8622d43e15
Herman Autore <herman@ufl.edu>
2024-06-24 18:05:50 -0400

    clean_logs: add `test_mode`, fix literal globs

--- src/scripts/clean_logs.bash
+++ src/scripts/clean_logs.bash
@@ -5,24 +5,78 @@
 # Runs from different programs do not have the same name or timestamp.
 # Dotfiles are not included in glob expansion.
 # Data and log directories have a fixed depth.
-# NOTE - DEVELOPMENT
-# You can remove commented code after initial commit.
+
+# shellcheck source="/data/herman/Documents/Git Repositories/Herman Code/Shell Package/functions/functions.bash"
+source "$HERMANS_CODE_INSTALL_PATH/Shell Package/functions/functions.bash" || exit 1
 
 # Formatting
 GRN=$'\e[0;32m'
 RED=$'\e[0;31m'
 NC=$'\e[0m'
 
+usage0() {
+    cat <<USAGE
+This script moves "data" and "log" files to a ".TRASH" folder if their corresponding \`nohup\` logs are not found in the "logs (nohup)" directory.
+
+"$(basename "$0")" -t [t|T[rue]]|[f|F[alse]]
+
+    -t \`test_mode\`, a boolean argument {T[RUE], F[ALSE]}. Case in-sensitive.
+USAGE
+}
+usage() {
+    usage0 1>&2
+    exit 1
+}
+
+while getopts ":t:" opt; do
+    case "${opt}" in
+        t) test_mode=${OPTARG};;
+        *) usage;;
+    esac
+done
+shift $((OPTIND -1))
+
+# >>> Argument confirmation >>>
+# `test_mode`
+shopt -s nocasematch &&
+if [[ "$test_mode" =~ (t|true) ||  "$test_mode" =~ (f|false) ]]; then
+    if [[ "$test_mode" =~ (t|true) ]]; then
+        test_mode_="true"
+    elif [[ "$test_mode" =~ (f|false) ]]; then
+        test_mode_="false"
+    else
+        echo "This should not happen"
+        exit 1
+    fi
+else
+    invalid_arguments=1
+fi &&
+shopt -u nocasematch 
+
+echo "The value of the argument 'test_mode' is '$test_mode'"
+
+if [[ "$invalid_arguments" == 1 ]]; then
+    echo "${RED}You provided invalid arguments.${NC}"
+    usage
+fi
+# <<< Argument confirmation <<<
+
 # set constants
 current_working_directory="$(pwd)"
 echo "\`current_working_directory\`: \"$current_working_directory\""
 
-# shellcheck source="/data/herman/Documents/Git Repositories/Herman Code/Shell Package/functions/functions.bash"
-source "$HERMANS_CODE_INSTALL_PATH/Shell Package/functions/functions.bash" || exit 1
+# Assert there is a valid `nohup` logs directory
+nohup_logs_directory="$current_working_directory/logs (nohup)"
+if [[ -d "$nohup_logs_directory" ]]; then
+    :
+else
+    echo "The directory \"logs (nohup)\" was not found in your current working directory."
+    exit 1
+fi
 
 # Collect completed `nohup` logs
 completed_jobs_array=()
-for file_path in "$current_working_directory/logs (nohup)"/*;
+for file_path in "$nohup_logs_directory"/*;
 do
     file_name="$(basename -- "$file_path")"
     run_name="${file_name%.*}"
@@ -40,13 +94,12 @@ done
 dir_path_logs="$current_working_directory/logs"
 dir_path_data_intermediate="$current_working_directory/data/intermediate"
 dir_path_data_output="$current_working_directory/data/output"
-# dir_path_array=("$dir_path_logs" \
-#                 "$dir_path_data_intermediate" \
-#                 "$dir_path_data_output")
 shopt -u dotglob  # Make sure we do not include dotfiles in our glob expansion.
+shopt -s nullglob  # Make glob return nulls instead of its literal self if it matches no patterns.
 list_of_paths_logs=("$dir_path_logs"/*/*)
 list_of_paths_data_intermediate=("$dir_path_data_intermediate"/*/*)
 list_of_paths_data_output=("$dir_path_data_output"/*/*)
+shopt -u nullglob
 all_files_to_check=()
 all_files_to_check+=("${list_of_paths_logs[@]}")
 all_files_to_check+=("${list_of_paths_data_intermediate[@]}")
@@ -54,7 +107,7 @@ all_files_to_check+=("${list_of_paths_data_output[@]}")
 
 files_to_remove=()
 echo "
-The complete collection of data directories and log files for all runs are below:"
+The complete collection of data directories and log files for all runs are below. ${RED}RED${NC} items will be deleted. ${GRN}Green${NC} items will be not be deleted."
 for file_path in "${all_files_to_check[@]}";
 do
     file_name="$(basename -- "$file_path")"
@@ -70,42 +123,10 @@ do
     fi
 done
 
-# # Recreate directory structure in trash folder: build paths
-# echo "
-# Recreate directory structure in trash folder: build paths"
-# trash_folder_paths_array=()
 trash_folder_path="$current_working_directory/.TRASH"
-# for dir_path in "${dir_path_array[@]}";
-# do
-#     echo "  \`dir_path\`: \"$dir_path\""
-#     dir_relative_path="$(realpath -s --relative-to="$trash_folder_path" "$dir_path")"
-#     tree_limb="${dir_relative_path##}"
-#     echo "    \`dir_relative_path\`: \"$dir_relative_path\""
-#     tree_limb="${dir_relative_path/"../"/}"  # HACK
-#     echo "    \`tree_limb\`: \"$tree_limb\""
-#     trash_sub_dir="$trash_folder_path/$tree_limb"
-#     echo "    \`trash_sub_dir\`: \"$trash_sub_dir\""
-#     trash_folder_paths_array+=("$trash_sub_dir")
-#     echo ""
-# done
-
-# # Recreate directory structure in trash folder: make directories
-# echo "
-# Recreating directory structure in trash folder: make directories."
-# for dir_path in "${trash_folder_paths_array[@]}";
-# do
-#     echo "  Working on \`dir_path\`: \"$(realpath --relative-to="$current_working_directory" "$dir_path")\""
-#     if [ -d "$dir_path" ];
-#     then
-#         echo "    Directory exists."
-#     else
-#         echo "    Creating directory."
-#         mkdir -p "$dir_path"
-#     fi
-# done
 
 # User confirmation
-read -pr "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
+read -r -p "Continue? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exit 1
 
 # Trash files and folders
 echo "
@@ -122,6 +143,11 @@ do
     to_dir="$(dirname -- "$to_path")"
     mkdir -p "$to_dir"
     echo "    \`to_path\`: \"$to_path\""
-    # mv "$file_path" "$to_dir"
+    if [[ "$test_mode_" == "true" ]]; then
+        echo "mv \"$file_path\" \"$to_dir\""
+    else
+        # mv "$file_path" "$to_dir"
+        :
+    fi
     echo ""
 done
